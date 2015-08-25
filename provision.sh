432b011fafe4a45d956a2cb72743afa4e766e037
Kristjan Jansen <kristjan.jansen@gmail.com>
2015-08-25 12:03:01 +0300

    sudofication

--- provision.sh
+++ provision.sh
@@ -11,7 +11,7 @@ sudo apt-get upgrade -y
 
 sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 sudo locale-gen en_US.UTF-8
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 
 # Requisites
@@ -22,32 +22,32 @@ sudo apt-get install curl -y
 sudo apt-get install unzip -y
 sudo apt-get install imagemagick -y
 
-apt-get install -y dos2unix gcc git libmcrypt4 libpcre3-dev 
+sudo apt-get install -y dos2unix gcc git libmcrypt4 libpcre3-dev 
 
-apt-add-repository ppa:nginx/stable -y
-apt-add-repository ppa:rwky/redis -y
-apt-add-repository ppa:chris-lea/node.js -y
-apt-add-repository ppa:ondrej/php5-5.6 -y
+sudo apt-add-repository ppa:nginx/stable -y
+sudo apt-add-repository ppa:rwky/redis -y
+sudo apt-add-repository ppa:chris-lea/node.js -y
+sudo apt-add-repository ppa:ondrej/php5-5.6 -y
 
 sudo apt-get update -y
 
 
 # PHP
 
-apt-get install -y php5-cli php5-mysqlnd php5-json php5-curl php5-gd php5-mcrypt php5-memcached php5-imagick
+sudo apt-get install -y php5-cli php5-mysqlnd php5-json php5-curl php5-gd php5-mcrypt php5-memcached php5-imagick
 
 
 # Make MCrypt Available
 
-ln -s /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available
+sudo ln -s /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available
 sudo php5enmod mcrypt
 
 
 # Composer
 
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-printf "\nPATH=\"/home/vagrant/.composer/vendor/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo printf "\nPATH=\"/home/vagrant/.composer/vendor/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
 
 
 # Set PHP CLI Settings
@@ -60,25 +60,25 @@ sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/cli/php.ini
 
 # Nginx and PHP-FPM
 
-apt-get install -y nginx php5-fpm
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
+sudo apt-get install -y nginx php5-fpm
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-available/default
 
 
 # Set Nginx settings
 
-sed -i "s/http {/http {\n\nclient_max_body_size 100M;/" /etc/nginx/nginx.conf
+sudo sed -i "s/http {/http {\n\nclient_max_body_size 100M;/" /etc/nginx/nginx.conf
 
 
 # Set PHP-FPM settings
 
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/fpm/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/fpm/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/fpm/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/fpm/php.ini
 
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php5/fpm/php.ini
+sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php5/fpm/php.ini
 
 
 # Restart
@@ -100,10 +100,10 @@ sudo sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/my
 sudo mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
 sudo mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
 
-# mysql --user="root" --password="secret" -e "CREATE USER 'server'@'0.0.0.0' IDENTIFIED BY 'secret';"
-# mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'server'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
-# mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'server'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
-# mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
+# sudo mysql --user="root" --password="secret" -e "CREATE USER 'server'@'0.0.0.0' IDENTIFIED BY 'secret';"
+# sudo mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'server'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+# sudo mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'server'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+# sudo mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
 
 sudo service mysql restart
 
@@ -139,7 +139,7 @@ sudo chown -R www-data:www-data /var/www
 sudo cp /vagrant/trip2.nginx /etc/nginx/sites-available/trip2
 sudo ln -fs /etc/nginx/sites-available/trip2 /etc/nginx/sites-enabled/trip2
 
-cp /vagrant/.htpasswd /etc/nginx/
+sudo cp /vagrant/.htpasswd /etc/nginx/
 
 # cd /var/www
 # git clone https://github.com/kristjanjansen/trip2.git
