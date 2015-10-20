2cc9dd779ba2529553724497e09fde32f4b19251
kristjanjansen <kristjan.jansen@gmail.com>
2015-10-20 16:08:46 +0300

    php7

--- provision.sh
+++ provision.sh
@@ -28,15 +28,17 @@ sudo apt-get install -y dos2unix gcc git libmcrypt4 libpcre3-dev
 sudo apt-add-repository ppa:nginx/stable -y
 sudo apt-add-repository ppa:rwky/redis -y
 sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-add-repository ppa:ondrej/php5-5.6 -y
+sudo apt-add-repository ppa:ondrej/php-7.0 -y
 
 sudo apt-get update -y
 
 
 # PHP
 
-sudo apt-get install -y php5-cli php5-mysqlnd php5-json php5-curl php5-gd php5-mcrypt php5-memcached php5-imagick
-
+sudo apt-get install -y --force-yes php7.0-cli php7.0-dev \
+php-pgsql php-sqlite3 php-gd \
+php-curl php7.0-dev \
+php-imap php-mysql
 
 # Make MCrypt Available
 
@@ -61,7 +63,7 @@ sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/cli/php.ini
 
 # Nginx and PHP-FPM
 
-sudo apt-get install -y nginx php5-fpm
+sudo apt-get install -y --force-yes nginx php7.0-fpm
 sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-available/default
 
@@ -69,22 +71,44 @@ sudo rm /etc/nginx/sites-available/default
 
 sudo sed -i "s/http {/http {\n\nclient_max_body_size 100M;/" /etc/nginx/nginx.conf
 
-
 # Set PHP-FPM settings
 
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php5/fpm/php.ini
-sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/fpm/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/fpm/php.ini
 
-sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
-sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php5/fpm/php.ini
+# Copy fastcgi_params to Nginx because they broke it on the PPA
 
+cat > /etc/nginx/fastcgi_params << EOF
+fastcgi_param   QUERY_STRING        \$query_string;
+fastcgi_param   REQUEST_METHOD      \$request_method;
+fastcgi_param   CONTENT_TYPE        \$content_type;
+fastcgi_param   CONTENT_LENGTH      \$content_length;
+fastcgi_param   SCRIPT_FILENAME     \$request_filename;
+fastcgi_param   SCRIPT_NAME     \$fastcgi_script_name;
+fastcgi_param   REQUEST_URI     \$request_uri;
+fastcgi_param   DOCUMENT_URI        \$document_uri;
+fastcgi_param   DOCUMENT_ROOT       \$document_root;
+fastcgi_param   SERVER_PROTOCOL     \$server_protocol;
+fastcgi_param   GATEWAY_INTERFACE   CGI/1.1;
+fastcgi_param   SERVER_SOFTWARE     nginx/\$nginx_version;
+fastcgi_param   REMOTE_ADDR     \$remote_addr;
+fastcgi_param   REMOTE_PORT     \$remote_port;
+fastcgi_param   SERVER_ADDR     \$server_addr;
+fastcgi_param   SERVER_PORT     \$server_port;
+fastcgi_param   SERVER_NAME     \$server_name;
+fastcgi_param   HTTPS           \$https if_not_empty;
+fastcgi_param   REDIRECT_STATUS     200;
+EOF
 
 # Restart
 
 sudo service nginx restart
-sudo service php5-fpm restart
+sudo service php7.0-fpm restart
 
 
 # Install MySQL
@@ -107,6 +131,10 @@ sudo mysql --user="root" --password="$1" -e "GRANT ALL ON *.* TO root@'%' IDENTI
 
 sudo service mysql restart
 
+# Add Timezone Support To MySQL
+
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=secret mysql
+
 # Node
 
 sudo apt-add-repository ppa:chris-lea/node.js -y
@@ -127,8 +155,6 @@ sudo apt-get install -y redis-server
 sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
-
-# You might need to to reenter this later 
 
 sudo swapon /swapfile
 
