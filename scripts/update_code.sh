cfdc523be34c67355dc0963ec87d6d5954e77702
kristjanjansen <kristjan.jansen@gmail.com>
2016-03-23 17:14:26 +0200

    small fix

--- scripts/update_code.sh
+++ scripts/update_code.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# env $(cat trip2/.env | xargs) slackcli -h servers -m "Starting updating code from Github master" -u server
-
 cd trip2
+
 git pull
-composer install --prefer-dist --no-interaction
+composer install --no-interaction
 npm install --no-bin-links
 gulp
+
 php artisan optimize --force
 php artisan cache:clear
 php artisan route:cache
 php artisan config:cache
-
-#cd ..
-#env $(cat trip2/.env | xargs) slackcli -h servers -m "Code updated from Github master" -u server
 
 
 
