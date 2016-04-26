3ce60524b1ad434c72ee2b7dde5859213a34a5e0
kristjanjansen <kristjan.jansen@gmail.com>
2016-04-26 22:59:58 +0300

    firewall tweak

--- provision.sh
+++ provision.sh
@@ -134,11 +134,12 @@ sudo /sbin/swapon /var/swap.1
 
 # Firewall
 
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
 sudo ufw enable
-# sudo ufw logging on
-sudo ufw allow 22
-sudo ufw allow 80
-sudo ufw allow 443
 
 # Configuring Nginx
 
