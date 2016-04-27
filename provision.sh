88dc043dab63761e84d278fc8144a948eaa9a1d9
kristjanjansen <kristjan.jansen@gmail.com>
2016-04-27 18:42:53 +0300

    fix

--- provision.sh
+++ provision.sh
@@ -198,8 +198,7 @@ if [ "$ENVIRONMENT" = "staging" ]; then
     sudo sed -i "s/nothing./nothing.\n\n\/usr\/sbin\/netdata\n/" /etc/rc.local
 
     echo "$ENVOYER_KEY" | sudo tee -a /home/tripikas/.ssh/authorized_keys > /dev/null
-    echo "tripikas ALL=NOPASSWD: /usr/sbin/service php7.0-fpm reload" | sudo tee -a /etc/sudoers.d/php-fpm > /dev/null
-
+    
     # Firewall
 
     sudo ufw default deny incoming
