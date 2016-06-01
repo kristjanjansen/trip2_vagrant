8cc4359aaa3411a1e9791551ecdd6439adce4f61
kristjanjansen <kristjan.jansen@gmail.com>
2016-06-01 19:42:55 +0300

    backup script

--- config/staging/backup.sh
+++ config/staging/backup.sh
@@ -7,10 +7,15 @@ else
     mysqladmin -uroot -p$1 -f drop trip2
     mysqladmin -uroot -p$1 create trip2
 
-    ssh root@trip.ee "mysqldump -uroot -p$2 trip2 | gzip > /var/www/backup/trip2.sql.gz"
-    scp root@trip.ee:/var/www/backup/trip2.sql.gz /var/www/backup/.
-    gunzip -c /var/www/backup/trip2.sql.gz | mysql -uroot -p$1 trip2
-    rm /var/www/backup/trip2.sql.gz
+    ssh root@46.101.120.198 "mysqldump -uroot -p$2 trip2 | gzip > $BACKUP_DIR/trip2.sql.gz"
+    scp root@46.101.120.198:/var/www/trip2.sql.gz /var/www/.
+    gunzip -c /var/www/trip2.sql.gz | mysql -uroot -p$1 trip2
+    mv /var/www/trip2.sql.gz /var/www/trip2--$(date +%F--%R).sql.gz
+
+    rsync -azP --delete root@46.101.120.198:/var/www/trip2/storage/app/images/ /var/www/trip2/storage/app
+    # tar -zcvf /var/www/trip2--$(date +%F--%R).images.gz /var/www/trip2/storage/app/images/original/ > /dev/null
+
+    find /var/www/*.sql.gz -type f -mtime +1 -delete
 
 fi
 
